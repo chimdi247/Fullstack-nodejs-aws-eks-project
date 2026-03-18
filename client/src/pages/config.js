334e52a050cb3b64645c15f8cc39c815b6d9c1e6
chimdi247 <Chimdi247@gmail.com>
2026-03-19 00:05:28 +0200

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,6 +1,6 @@
 
 // // const API_BASE_URL = "http://localhost:8800";
-const API_BASE_URL = "http://aluru.site";
+const API_BASE_URL = "https://books.raph247.online";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 // export default API_BASE_URL;
